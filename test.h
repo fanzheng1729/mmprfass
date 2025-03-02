9637d6a14f0200d905253c1bf72d03b4ff8a3bc3
fanzheng1729 <58038971+fanzheng1729@users.noreply.github.com>
2025-03-02 12:42:39 +0100

    test

--- test.h
+++ test.h
@@ -13,10 +13,10 @@ unsigned testsat2(unsigned n); // should be 0
 
 bool testDAG(unsigned n); // should be 1
 
+bool testMCTS(std::size_t sizelimit, double const exploration[2]); // should be 1
+
 // Check if iterators to assertions are sorted in ascending number.
 bool checkassiters
     (Assertions const & assertions, Assiters const & assiters); // should be 1
-
-bool testMCTS(std::size_t sizelimit, double const exploration[2]); // should be 1
 
 #endif // TEST_H_INCLUDED
